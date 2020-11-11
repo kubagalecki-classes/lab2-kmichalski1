0dab9409bcf563486dd7afeb1c37ea32eb17949e
kmichalski1 <73621723+kmichalski1@users.noreply.github.com>
2020-11-11 20:04:21 +0100

    Update solution

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -1,8 +1,46 @@
 #pragma once
-
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* ptr;
+
+public:  
+    ResourceManager()
+    { 
+        ptr = new Resource; 
+    }
+    ResourceManager(const ResourceManager& res) : ptr(res.ptr) 
+    {
+        ptr = new Resource{ *res.ptr }; 
+    }
+    ResourceManager& operator=(const ResourceManager& res) 
+    {
+        if (ptr == res.ptr) return *this;
+        delete ptr;
+        ptr = new Resource{ *res.ptr };
+        return *this;
+    }
+    ResourceManager(ResourceManager&& res) 
+    {
+        ptr = res.ptr;
+        res.ptr = nullptr;
+    }
+    ResourceManager& operator=(ResourceManager&& res) 
+    {
+        if (ptr == res.ptr) return *this;
+        delete ptr;
+        ptr = res.ptr;
+        res.ptr = nullptr;
+        return *this;
+    }
+    double get() 
+    { 
+        return ptr->get(); 
+    }
+    ~ResourceManager()
+    { 
+        delete ptr; 
+    }
 };
